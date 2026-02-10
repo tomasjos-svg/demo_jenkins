9b5f13662acbcaa7469a5f969a02c1af4da0e6d2
tomas <tomasjhn@educastur.org>
2026-02-10 21:23:21 +0100

    Proyecto Maven para prueba de Jenkins

---
+++ src/main/java/com/ejemplo/demo_jenkins/App.java
@@ -0,0 +1,13 @@
+package com.ejemplo.demo_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
